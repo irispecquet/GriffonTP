1dfdca185f982f28b26a8f89a39c56ae23b14972
Iris Pecquet <irispecquet@icloud.com>
2024-11-19 10:14:43 +0100

    [Data] - Create SO CardData with base attributes

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardData", menuName = "Card Data", order = 0)]
+public class CardData : ScriptableObject
+{
+    [field:SerializeField] public string Name { get; private set; }
+    [field:SerializeField] public Consumption Consumption { get; private set; }
+    [field:SerializeField] public NegativeEffect[] NegativeEffects { get; private set; }
+    // Condition[]
+}
+
+public enum Consumption
+{
+    Food,
+    Beer,
+}
+
+public enum NegativeEffect
+{
+    Fight,
+    Smell,
+    Loud
+}
